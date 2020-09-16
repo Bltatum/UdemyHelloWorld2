fc7b43f94a0b8ff0aac1775dc01ed13c913ce76c
brennen <bmar566@gmail.com>
2020-09-16 13:14:33 -0500

    finished master detail tutorial

---
+++ UdemyHelloWorld2/ContactDetailPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UdemyHelloWorld2.Models;
+using Xamarin.Forms;
+
+namespace UdemyHelloWorld2
+{
+	public partial class ContactDetailPage : ContentPage
+	{
+		public ContactDetailPage(Contact contact)
+		{
+			if (contact == null)
+				throw new ArgumentNullException();
+
+			BindingContext = contact;
+
+			InitializeComponent();
+		}
+	}
+}
