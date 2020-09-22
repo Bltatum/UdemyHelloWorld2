ca1eebd73d88c87925d7f0db1b74f5ef3284fe97
brennen <bmar566@gmail.com>
2020-09-22 15:52:42 -0500

    Completed forms and setting pages tutorial

---
+++ UdemyHelloWorld2/ContactsMethods.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UdemyHelloWorld2
+{
+    public partial class ContactsMethods : ContentPage
+    {
+        public ContactsMethods()
+        {
+            InitializeComponent();
+
+            listview.ItemsSource = new List<string>
+            {
+                "None",
+                "Email",
+                "SMS"
+            };
+        }
+
+        public ListView ContactMethods { get { return listview; } }
+    }
+}
---
+++ UdemyHelloWorld2/ToggledPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UdemyHelloWorld2
+{
+    public partial class ToggledPage : ContentPage
+    {
+        public ToggledPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
